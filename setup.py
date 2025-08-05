b01ad156fed9697f39d41ef442dc458a7378f80a
gtluu <46392631+gtluu@users.noreply.github.com>
2025-08-05 14:13:13 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='scilslab_pyworld2px',
-      version='0.1.0',
+      version='0.1.1',
       description='A Python implementation for converting Bruker world coordinates to x-y coordinates',
       url='https://github.com/gtluu/scilslab_pyworld2px',
       license='Apache License 2.0',
       author='Gordon T. Luu',
       author_email='gtluu912@gmail.com',
-      packages=['util'],
+      packages=['scilslab_pyworld2px'],
       install_requires=['numpy', 'pandas'])
+
