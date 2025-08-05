3a7b6677e0835f45ec152076ca0050264fed41a6
gtluu <46392631+gtluu@users.noreply.github.com>
2025-08-05 14:15:23 -0500

    Update __init__.py

--- scilslab_pyworld2px/__init__.py
+++ scilslab_pyworld2px/__init__.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
 
-from util.coords import *
-from util.world2px import *
+from scilslab_pyworld2px.coords import *
+from scilslab_pyworld2px.world2px import *
+
